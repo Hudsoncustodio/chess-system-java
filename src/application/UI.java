0c0a34ba271c3430dd02b154e44560dc0c44d616
Hudson Ferreira <141644371+Hudsoncustodio@users.noreply.github.com>
2023-12-20 12:52:03 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,6 +58,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if(chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -109,7 +112,7 @@ public class UI {
 		System.out.print(ANSI_RESET);
 		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
-		System.out.print(Arrays.toString(white.toArray()));
+		System.out.print(Arrays.toString(black.toArray()));
 		System.out.print(ANSI_RESET);
 	}
 }
